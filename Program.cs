199d1724763a202469e594a7141ba368d314f8bd
Philip Graham <GrahPhil963@GoTVDSB.ca>
2022-05-18 09:56:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Monogame_Animation_PHILIP_GRAHAM
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
